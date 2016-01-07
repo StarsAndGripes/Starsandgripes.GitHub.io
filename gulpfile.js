f9a68aaf4f12c339fdd7c1bef8d88a22f4163237
Melissa <melpatenator@gmail.com>
2016-01-07 12:35:59 -0600

    add haml stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
+var browserSync = require('browser-sync');
 var gulp = require('gulp');
+var haml = require('gulp-haml');
 var sass = require('gulp-sass');
 
 // compile scss files
@@ -6,8 +8,32 @@ gulp.task('sass', function () {
   return gulp.src('app/assets/scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('app/assets/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('watch', function () {
+gulp.task('haml', function () {
+  return gulp.src('app/**/*.haml')
+    .pipe(haml())
+    .pipe(gulp.dest('app'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+//browser sync
+gulp.task('browserSync', function () {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+gulp.task('watch', ['browserSync', 'haml', 'sass'], function () {
   gulp.watch('app/assets/scss/**/*.scss', ['sass']);
+  gulp.watch('app/**/*.haml`', ['haml']);
+  gulp.watch('app/*.html`', browserSync.reload);
+  gulp.watch('app/js/**/*.js`', browserSync.reload);
 });
