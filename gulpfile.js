be31e5aa9e4080538a686c55b4c8edf97566678b
Melissa <melpatenator@gmail.com>
2016-01-07 12:13:24 -0600

    add watch task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+// compile scss files
 gulp.task('sass', function () {
-  return gulp.src('app/assets/scss/style.scss')
+  return gulp.src('app/assets/scss/**/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('app/css'))
+    .pipe(gulp.dest('app/assets/css'))
+});
+
+gulp.task('watch', function () {
+  gulp.watch('app/assets/scss/**/*.scss', ['sass']);
 });
